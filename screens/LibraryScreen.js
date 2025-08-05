b8808457f87f71b1c282bcbdd1a5bfe7a861859d
Your name <Your email>
2025-08-05 16:24:23 +0200

    Yolo2

--- screens/LibraryScreen.js
+++ screens/LibraryScreen.js
@@ -14,25 +14,25 @@ export default function LibraryScreen({navigation}) {
    <View style={styles.container}>
                <Text>Ma Bibliothèque</Text>
    
-               <TouchableOpacity style={styles.input} onPress={() => navigation.navigate("mangas")}>
+               <TouchableOpacity style={styles.input} onPress={() => navigation.navigate("BookLibraryScreen", { category: "mangas" })}>
                    <Text style={styles.text}>{mangas}</Text>
-                   {/*<Image source={require('')} style={styles.logo1} />*/}
+                   <Image source={require('../../assets/images/BibliothequeManga.png')} style={styles.logo1}/>
                </TouchableOpacity>
    
-               <TouchableOpacity style={styles.input} onPress={() => navigation.navigate("bd")}>
+               <TouchableOpacity style={styles.input} onPress={() => navigation.navigate("BookLibraryScreen", { category: "bd" })}>
                    <Text style={styles.text}>{bd}</Text>
-                   {/*<Image source={require('')} style={styles.logo2} />*/}
+                   <Image source={require('../../assets/images/BibliothequeBD.png')} style={styles.logo2} />
                </TouchableOpacity>
    
-               <TouchableOpacity style={styles.input} onPress={() => navigation.navigate("livre")}>
+               <TouchableOpacity style={styles.input} onPress={() => navigation.navigate("BookLibraryScreen", { category: "livre" })}>
                    <Text style={styles.text}>{livre}</Text>
-                   {/*Image source={require('')} style={styles.logo3} />*/}
+                   <Image source={require('../../assets/images/BibliothequeLivre.png')} style={styles.logo3} />
                </TouchableOpacity>
    
                <StatusBar style="auto" />
            </View>
-       )
-   }
+  )}
+   
 
 const styles = StyleSheet.create({
     container: {
